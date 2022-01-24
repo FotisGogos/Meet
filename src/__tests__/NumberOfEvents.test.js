da379e7da69b78eea47fcecd217acbb366c3c9a3
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2022-01-24 17:12:31 +0100

    4.3 Test file is created for NumberOfEvents.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents > component', () => {
+
+  test('textbox is rendered correctly', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents')).toHaveLength(1);
+  })
+
+  test('Textbox changes state of numberofevents', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    const newNumber = { target: { value: '16' } };
+    NumberOfEventsWrapper.find('.newNumber').simulate('change', newNumber);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(16);
+  })
+})
+ 
