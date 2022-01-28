15dffc39570c0d377d7ad960786cab0e66759f55
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2022-01-28 21:22:20 +0100

    4.4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import { mockData } from './mock-data';
+import { extractLocations, getEvents } from './api';
+import './nprogress.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <NumberOfEvents />
-      <EventList events={mockData} />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    
+  }
+
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  } 
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,25 +3,40 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
+    if(suggestions.length === 0) {
+        this.setState({ 
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city.',
+        showSuggestions: false
     });
-  };
+} else {
+    return this.setState({
+        query: value,
+        suggestions,
+        infoText:'',
+        showSuggestions: true
+    });
+}
+};
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+        query: suggestion,
+        showSuggestions: false,
+        infoText: ''
     });
-  }
+
+    this.props.updateEvents(suggestion);
+};
 
   render() {
     return (
@@ -31,11 +46,13 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
-        <ul className="suggestions">
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+          
           {this.state.suggestions.map((suggestion) => (
             <li key={suggestion}
-                onClick={() => this.handleItemClicked(suggestion)}
+                onClick={() => this.handleItemClicked("all")}
             >
               {suggestion}
             </li>
