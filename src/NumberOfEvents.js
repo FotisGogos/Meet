3b40984b9a5cdc5cdd933cc93995875dd95c731e
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2022-01-30 17:54:05 +0100

    4.4. React bootstrap

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
-  state = {
-    numberOfEvents: 16,
-  }
-
-  handleInputChanged = (event) => {
-    const value = event.target.value;
-    if (value < 1 || value > 16) {
-      this.setState({
-        numberOfEvents: value,
-      })
-    }
-  };
 
   render() {
     return (
@@ -21,10 +9,14 @@ class NumberOfEvents extends Component {
           type="number"
           className="newNumber"
           value={this.props.numberOfEvents}
-          onChange={(e) => this.handleInputChanged(e)}
+          onChange={(e) => this.props.updateNumberOfEvents(e)}
         />
       </div>
     )
   }
 }
 export default NumberOfEvents;
+
+
+
+
