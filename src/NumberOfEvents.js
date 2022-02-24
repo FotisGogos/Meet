4879359baa619bcde29d1b63679dd6c42bf6760b
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2022-02-24 17:22:48 +0100

    4.7 ErrorAlert (errorText)

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+
 
 class NumberOfEvents extends Component {
       
   render() {
     return (
       <div className="NumberOfEvents">
+       <ErrorAlert text={this.props.errorText} />
         <p>Number of events</p>
         <input
           type="number"
@@ -12,6 +15,8 @@ class NumberOfEvents extends Component {
           value={this.props.numberOfEvents}
           onChange={(e) => this.props.updateNumberOfEvents(e)}
         />
+        
+
       </div>
     )
   };
